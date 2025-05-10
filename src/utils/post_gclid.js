a897745bb5df757e820f95a10505bb38b8b0859d
Ali-Dev <ali.dev11@gmail.com>
2025-05-11 00:23:21 +0500

    feat: Added more custom headers

--- src/utils/post_gclid.js
+++ src/utils/post_gclid.js
@@ -1,25 +1,17 @@
-import { BASE_URL, postToken } from "@/utils/axios_instance";
+import { clientAxios } from '@/utils/axios_clients';
 
 export async function postGclid(url = "", gclid) {
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${postToken}`,
-    },
-    body: JSON.stringify({
+  try {
+    const payload = {
       data: {
         gclid: gclid,
       },
-    }),
-  };
+    };
 
-  const res = await fetch(`${BASE_URL}/${url}`, requestOptions);
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
+    const response = await clientAxios.post(url, payload);
+    return response.data;
+  } catch (error) {
+    console.error("Error posting GCLID:", error);
+    throw new Error("Failed to submit GCLID");
   }
-
-  const repo = await res.json();
-  return repo;
 }
