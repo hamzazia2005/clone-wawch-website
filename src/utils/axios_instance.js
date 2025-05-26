319d208d983731a2600473aece0bf61bf0aa6eb6
Ali-Dev <ali.dev11@gmail.com>
2025-05-26 15:05:49 +0500

    chore: format fixes 2

--- src/utils/axios_instance.js
+++ src/utils/axios_instance.js
@@ -1,4 +1,4 @@
 export const BASE_URL = process.env.STRAPI_BE_URL;
 export const getToken = process.env.STRAPI_ACCESS_TOKEN;
-export const postToken = process.env.STRAPI_POST_TOKEN;
+export const postToken = process.env.NEXT_PUBLIC_STRAPI_POST_TOKEN;
 export const isLocal = false;
