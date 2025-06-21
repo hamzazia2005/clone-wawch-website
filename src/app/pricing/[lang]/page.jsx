b1e23552ff97d0b0dbfe4355eba248b52cb4d710
schwert-gottes <saifullah.alam552@gmail.com>
2025-06-21 15:35:05 +0500

    Change price in different lang

--- src/app/pricing/[lang]/page.jsx
+++ src/app/pricing/[lang]/page.jsx
@@ -44,7 +44,9 @@ const Page = async ({ params }) => {
 
   const updatedPricing = {
     ...pricing,
-    prices: pricing.prices.filter((plan) => plan.price_yearly !== undefined),
+    prices: pricing.prices.filter(
+      (plan) => plan.new_price_yearly !== undefined
+    ),
   };
 
   const jsonLd = {
@@ -63,7 +65,7 @@ const Page = async ({ params }) => {
       "@type": "Offer",
       name: plan.title,
       description: plan.desc,
-      price: plan.price_yearly,
+      price: plan.new_price_yearly,
       priceCurrency: plan.currency || "USD",
       availability: "https://schema.org/InStock",
     })),
