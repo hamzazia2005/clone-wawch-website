e9d601a2a778a489d46b065ec2417369e86dcd02
Pakizah Saqib <pakizahsaqib@gmail.com>
2025-09-03 12:55:29 +0500

    fix: rebased | fixed conflicts

--- src/views/feature/feature.jsx
+++ src/views/feature/feature.jsx
@@ -20,8 +20,8 @@ const Detail = ({ data, heading }) => {
         {data?.map((item, index) => (
           <PopUp key={index} isBounce={true} duration={1}>
             <Link
-              href={`/${lang}/feature/${item?.attributes?.slug}`}
-              className='flex flex-col md:flex-row gap-8 p-2 sm:p-4 mb-8 border border-gray-300 scale-1 hover:scale-[1.025] hover:shadow-[0px_0px_20px_2px_#dcdcdc] transition-all rounded-lg'
+              href={lang? `/${lang}/feature/${item?.attributes?.slug}`:`/feature/${item?.attributes?.slug}`}
+              className='flex flex-col gap-8 p-2 sm:p-4 mb-8 border border-gray-300 scale-1 hover:scale-[1.025] hover:shadow-[0px_0px_20px_2px_#dcdcdc] transition-all rounded-lg'
             >
               <Image
                 src={
