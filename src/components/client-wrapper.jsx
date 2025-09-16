a0570f4dfa06809ecbb6f34b2c00cdb4ba7075a7
Pakizah Saqib <pakizahsaqib@gmail.com>
2025-09-16 12:31:55 +0500

    feat: added aosinit as dynamic import

--- src/components/client-wrapper.jsx
+++ src/components/client-wrapper.jsx
@@ -3,7 +3,11 @@ import { useEffect, useState, Suspense } from "react";
 import WhatsAppButton from "./whatsapp-button";
 import { AppWrapper } from "@/context";
 import Loader from "./loader";
-//import AOSinit from "@/animations/AOSinit";
+import dynamic from "next/dynamic";
+
+const AOSinit = dynamic(() => import('@/animations/AOSinit'), {
+  ssr: false
+});
 
 export default function ClientEnhancer({ children }) {
   const [jsOn, setJsOn] = useState(false);
@@ -18,7 +22,7 @@ export default function ClientEnhancer({ children }) {
 
   return (
     <Suspense fallback={<Loader />}>
-      {/* <AOSinit/> */}
+      <AOSinit/>
       <AppWrapper>
         {children}
         <WhatsAppButton /> 
