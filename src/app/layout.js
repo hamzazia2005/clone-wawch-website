48ea1cfe1c4b0d30fb89621a83820848421c4c68
Pakizah Saqib <pakizahsaqib@gmail.com>
2025-09-19 12:50:05 +0500

    fix: uncommented the favicon from layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -32,14 +32,14 @@ export const metadata = {
   title: 'WAWCD: WhatsApp CRM with Contact Saver, Broadcasting & more',
   description:
     'Maximize Your Impact with the Most Powerful WhatsApp Solution! Enjoy Privacy Features, Zero Inbox, Contact Saver for WhatsApp, Broadcasting, and Seamless Integration with ChatGPT, Hubspot, and More – All in One WhatsApp Solution.',
-  // icons: {
-  //   icon: [
-  //     {
-  //       url: '/assets/logo.svg',
-  //       href: '/assets/logo.svg',
-  //     },
-  //   ],
-  // },
+    icons: {
+      icon: [
+        {
+          url: '/assets/logo.svg',
+          href: '/assets/logo.svg',
+        },
+      ],
+    },
 };
 
 export default function Layout({ children }) {
